0fe840df0726b76695c8575675c91e6985bc6625
pgzzl <65213498+pgzzl@users.noreply.github.com>
2021-03-23 16:18:50 +0800

    Add files via upload

---
+++ Homework4/Clock/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ClockTest
+{
+   
+    class Clock
+    {
+        private static DateTime currentTime = DateTime.Now;
+        private  static int hour;
+        private static int minute;
+        private static int second;
+        public static System.Timers.Timer aTimer;
+
+        public static void setAlarm(int hour, int minute, int second)
+        {
+            Console.WriteLine($"currentHour:{currentTime.Hour}currentMinute:{currentTime.Minute}currentSecond:{currentTime.Second}");
+            if (hour < 24 && hour >= 0 && minute >= 0 && minute < 60 && second >= 0 && second < 60)
+            {
+                Clock.hour = hour;
+                Clock.minute = minute;
+                Clock.second = second;
+            }
+            else
+            {
+                Console.WriteLine("设置alarm错误");
+                Clock.hour = Clock.minute = Clock.second = -1;
+            }
+           
+
+        }
+        public static void ClockBegin()
+        {
+
+            aTimer = new System.Timers.Timer(1000);
+            aTimer.Elapsed += Tick;
+            aTimer.Elapsed +=  Alarm;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        public static void Tick(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Tick ");
+            
+        }
+
+        public static void Alarm(Object source, ElapsedEventArgs e)
+        {
+            if (currentTime.Second == second&&currentTime.Minute==minute&&currentTime.Hour==hour)
+                Console.WriteLine("Alarm!!Alarm!! ");
+        }
+
+    }
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            int newHour;
+            int newMinute;
+            int newSecond;
+            Console.WriteLine("输入小时：分钟：秒 来设置闹钟时间");
+            newHour = int.Parse(Console.ReadLine());
+            newMinute = int.Parse(Console.ReadLine());
+            newSecond = int.Parse(Console.ReadLine());
+            Clock.setAlarm(newHour, newMinute, newSecond);
+            Clock.ClockBegin();
+            Console.ReadLine();
+            Clock.aTimer.Stop();
+            Clock.aTimer.Dispose();
+           
+
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Homework4/question1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1
+{
+   
+    public class Node<T> where T : IComparable
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data=t;
+        }
+        /*public static  T CompareGeneric(T t1,T t2)
+        {
+            if(t1.CompareTo(t2)>0)
+            {
+                return t1;
+            }
+            else
+            {
+                return t2;
+            }
+        }*/
+    }
+    public class GenericList<T> where T : IComparable
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head =tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        //类似ForEach
+      
+        public void Traverse(Action<T> a)
+        {
+            if (head == null)
+                return;
+            Node<T> node = head;
+            while(node!=null)
+            {
+                a(node.Data);
+                node = node.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum=0;
+            int max=0;
+            int min = 1;
+            GenericList<int> list = new GenericList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(4);
+            list.Add(3);;
+            list.Traverse(data=> { sum += data; });
+            Console.WriteLine($"和：{sum}");
+            list.Traverse(data => max=max<data ?data:max);
+            Console.WriteLine($"最大值：{max}");
+            list.Traverse(data => min = min > data ? data : min);
+            Console.WriteLine($"最小值：{min}");
+        }
+    }
+}
