f71b9260063cc1ac93b6ed19b6df014f2901a075
zzl <1104578688@qq.com>
2021-05-09 14:55:35 +0800

    homework10

---
+++ Homework10/Homework10/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework9
+{
+    public partial class Form1 : Form
+    {
+        SimpleCrawler myCrawler = new SimpleCrawler();
+       
+        public string strURL { get; set; }
+        public string display { get; set; }
+        //myCrawler.refreshTxt = func;
+        public Form1()
+        {
+           // strURL = "https://www.baidu.com/?tn=44048691_1_oem_dg";
+            InitializeComponent();
+           // strURL ="http://www.baidu.com";
+            textBoxURL.DataBindings.Add("Text", this, "strURL");
+            myCrawler.refreshTxt += func;
+            //myCrawler.d1 = new downloading(this.func);
+          //  listBox1.DataSource = myCrawler.bs;
+        }
+        public void func() {
+            if (listBox1.InvokeRequired)
+            {
+                Action myaction = new Action(()=>listBox1.Items.Add(myCrawler.item.Last()));
+                this.Invoke(myaction,null);
+            }
+            else
+                listBox1.Text += myCrawler.item.Last();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            /*  textBoxCrawel.DataBindings.Clear();
+              textBoxCrawel.DataBindings.Add("Text", thi()=>s.myCrawler, "strDisplay");*/
+            //new Thread(()=>myCrawler.Begin(strURL)).Start();
+            myCrawler.urls.Add(strURL, false);//加入初始页面
+            Thread myThread1 = new Thread(myCrawler.Crawl);
+            //Thread myThread2 = new Thread(myCrawler.Crawl);
+            myThread1.Start();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // textBoxURL.DataBindings.Add("Text", this, "strURL");
+           // textBoxCrawel.DataBindings.Add("Text", this.myCrawler, "strDisplay");
+            
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            
+           // bs.ResetBindings(false);
+            
+        }
+
+        private void textBoxCrawel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Homework10/Homework10/SimpleCrawler.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework9
+{
+    
+    class SimpleCrawler
+    {
+        public static string strDisplay { get; set; }
+        public List<string> item = new List<string>();
+        public Hashtable urls = new Hashtable();
+        private int count = 0;
+        //public downloading d1=new downloading();
+        public Action refreshTxt;
+
+        public void Begin(string strURL)
+        {
+           
+            strDisplay = "say something";
+           // myCrawler.bs.DataSource = myCrawler.item;
+            //string startUrl = "http://www.cnblogs.com/dstang2000/";
+            // if (args.Length >= 1) strURL = args[0];
+            //urls.Add(strURL, false);//加入初始页面
+           // Thread myThread = new Thread(myCrawler.Crawl);
+           // myThread.Start();
+        }
+        //public void func1() { }
+
+        public void Crawl()
+        {
+            //this.strDisplay="开始爬行了.... ";
+            Console.WriteLine("开始爬行了");
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url] && !Regex.IsMatch(url, "html|aspx|jsp"))
+                    {
+                        continue;
+                    }
+                    current = url;
+                }
+
+                if (current == null || count > 10) break;
+
+                //this.strDisplay += "爬行";
+                //this.strDisplay += current;
+                //this.strDisplay+="页面!";
+                Console.WriteLine("爬行" + current + "页面！");
+                strDisplay += current + "\n";
+                item.Add(current);
+                printItem();
+                refreshTxt();
+                //bs.ResetBindings(false);
+                string html = DownLoad(current); // 下载
+                urls[current] = true;
+                count++;
+                Parse(html, current);//解析,并加入新的链接
+                                     //this.strDisplay+="爬行结束";
+
+            }
+            Console.WriteLine("爬行结束str:" + strDisplay);
+        }
+        public void printItem() {
+            foreach (string s in item) {
+                Console.WriteLine(s+"\t");
+            }
+        }
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        public void Parse(string html, string current)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>');
+                if (strRef.Length == 0) continue;
+                if (urls[strRef] == null) urls[strRef] = false;
+            }
+        }
+    }
+}
